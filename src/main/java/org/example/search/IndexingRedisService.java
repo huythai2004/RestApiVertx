5cd7eef488b63d744d3fae5a4776fbc6fcc3c9a6
thai <thaiphamhuy3@gmail.com>
2025-07-22 10:04:31 +0700

    update indexingRedisService

--- src/main/java/org/example/search/IndexingRedisService.java
+++ src/main/java/org/example/search/IndexingRedisService.java
@@ -5,8 +5,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.example.service.RedisService;
 import redis.clients.jedis.JedisPooled;
+import redis.clients.jedis.params.ScanParams;
+import redis.clients.jedis.resps.ScanResult;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class IndexingRedisService {
     private static final Logger LOG = LogManager.getLogger(IndexingRedisService.class);
@@ -53,6 +58,32 @@ public class IndexingRedisService {
         return response;
     }
 
+    // Initialize categories indexing
+    public int initCategoriesIndexing() {
+
+        final  CategoyRediSearch categoyRediSearch = new CategoyRediSearch();
+        int result = categoyRediSearch.initAllIndexing();
+        LOG.info("Categories indexing result: {}", result);
+        return  result;
+    }
+
+    // Initialize packages indexing
+    public int initPackagesIndexing() {
+        final PackageRediSearch packageRediSearch = new PackageRediSearch();
+        int result = packageRediSearch.initAllIndexing();
+        LOG.info("Packages indexing result: {}", result);
+        return result;
+    }
+
+    // Initialize stickers indexing
+    public int initStickersIndexing() {
+        final StickerRediSearch stickerRediSearch = new StickerRediSearch();
+        int result = stickerRediSearch.initAllIndexing();
+        LOG.info("Stickers indexing result: {}", result);
+        return result;
+    }
+
+    //Mark the index as loaded
     private void markIndexAsLoaded(String indexKey) {
         try {
             jedisPooled.set(indexKey, "1");
@@ -61,6 +92,7 @@ public class IndexingRedisService {
         }
     }
 
+    //Check if the index exists
     private boolean isIndexExists(String indexKey) {
         try {
             String value = jedisPooled.get(indexKey);
@@ -71,25 +103,54 @@ public class IndexingRedisService {
         }
     }
 
-    public int initCategoriesIndexing(){
-        final  CategoyRediSearch categoyRediSearch = new CategoyRediSearch();
-        int result = categoyRediSearch.initAllIndexing();
-        LOG.info("Categories indexing result: {}", result);
-        return  result;
+    public  void clearIndexing() {
+        try {
+            //Delete index loading flags
+            jedisPooled.del(CATEGORIES_INDEX_KEY);
+            jedisPooled.del(PACKAGES_INDEX_KEY);
+            jedisPooled.del(STICKERS_INDEX_KEY);
+
+            //Delete all indexed data
+            String categoriesIndex = CATEGORIES_INDEX_KEY + ":*";
+            String packagesIndex = PACKAGES_INDEX_KEY + ":*";
+            String stickersIndex = STICKERS_INDEX_KEY + ":*";
+            List<String> prefixes = new ArrayList<>(Arrays.asList(categoriesIndex, packagesIndex, stickersIndex));
+            for (String prefix : prefixes) {
+                ScanParams scanParams = new ScanParams().match(prefix).count(1000);
+                String cursor = ScanParams.SCAN_POINTER_START;
+                deleteIndex(jedisPooled, scanParams, cursor);
+            }
+
+            //Drop all indexes
+            try {
+                jedisPooled.ftDropIndex(CATEGORIES_INDEX_KEY);
+            } catch (Exception e) {
+                LOG.warn("Error deleting categories index: {}", e.getMessage());
+            }
+
+            try {
+                jedisPooled.ftDropIndex(PACKAGES_INDEX_KEY);
+            } catch (Exception e) {
+                LOG.warn("Error deleting packages index: {}", e.getMessage());
+            }
+
+            try {
+                jedisPooled.ftDropIndex(STICKERS_INDEX_KEY);
+            } catch (Exception e) {
+                LOG.warn("Error deleting stickers index: {}", e.getMessage());
+            }
+        } catch (Exception e) {
+            LOG.error("Error clearing indexing", e.getMessage());
+        }
     }
 
-    public int initPackagesIndexing() {
-        final PackageRediSearch packageRediSearch = new PackageRediSearch();
-        int result = packageRediSearch.initAllIndexing();
-        LOG.info("Packages indexing result: {}", result);
-        return result;
+    private void deleteIndex(JedisPooled jedisPooled, ScanParams scanParams, String cursor) {
+        do {
+            ScanResult<String> scanResult = jedisPooled.scan(cursor, scanParams);
+            cursor = scanResult.getCursor();
+            if (!scanResult.getResult().isEmpty()) {
+                jedisPooled.del(scanResult.getResult().toArray(new String[0]));
+            }
+        } while (!cursor.equals(ScanParams.SCAN_POINTER_START));
     }
-
-    public int initStickersIndexing() {
-        final StickerRediSearch stickerRediSearch = new StickerRediSearch();
-        int result = stickerRediSearch.initAllIndexing();
-        LOG.info("Stickers indexing result: {}", result);
-        return result;
-    }
-
 }
